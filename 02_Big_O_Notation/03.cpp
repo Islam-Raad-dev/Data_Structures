c1cc42ddf535b7dd52ab172e1023d7d817bbcd64
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-31 19:15:19 +0300

    Edit : 16

--- 02_Big_O_Notation/03.cpp
+++ 02_Big_O_Notation/03.cpp
@@ -1,8 +1,8 @@
-#include <iostream>                
+#include <iostream>
 using namespace std;
 
-int main()     
-{      
+int main()
+{
     int n;
     cout << "Enter the value of n: ";
     cin >> n;
@@ -10,10 +10,10 @@ int main()
     // O(log n) - Logarithmic Time Complexity
     cout << "O(log n) - Logarithmic Time Complexity:" << endl;
     for (int i = 1; i < n; i *= 2)
-    {      
-        cout << i << " ";                  
-    }     
-    cout << endl;              
+    {
+        cout << i << " ";
+    }
+    cout << endl;
 
     return 0;
 }
