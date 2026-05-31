d2dc4269338f7689d7a7d7a66c1b58f816c89307
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-31 19:16:34 +0300

    Edit : 21

---
+++ 02_Big_O_Notation/04.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    // O(n log n) - Linearithmic Time Complexity
+
+    cout << "O(n log n) - Linearithmic Time Complexity:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n; j *= 2)
+        {
+            cout << "(" << i << ", " << j << ") ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
