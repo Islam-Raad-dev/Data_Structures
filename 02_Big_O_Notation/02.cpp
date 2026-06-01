4364bf1b557465d98da3ee71bb5391c8c413b527
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-01 08:58:19 +0300

    Edit :

--- 02_Big_O_Notation/02.cpp
+++ 02_Big_O_Notation/02.cpp
@@ -4,31 +4,6 @@ using namespace std;
                     
 int main()     
 {      
-    int n;
-    cout << "Enter the value of n: ";
-    cin >> n;
-
-    // O(n) - Linear Time Complexity
-    cout << "O(n) - Linear Time Complexity:" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << i << " ";                  
-    }     
-    cout << endl;              
-
-    // O(n^2) - Quadratic Time Complexity
-
-    
-    cout << "O(n^2) - Quadratic Time Complexity:" << endl;
-    for (int i = 0; i < n; i++)
-    {      
-        for (int j = 0; j < n; j++)
-        {      
-            cout << "(" << i << ", " << j << ") ";
-        }     
-
-        cout << endl;
-    }
 
     return 0;
 }
