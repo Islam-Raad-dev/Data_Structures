71d527ea2d7d918df83b2a9e885ed8eb54d8cbae
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-01 19:21:10 +0300

    Edit : 13

--- 03_Binary_Array_and_Matrix/08.cpp
+++ 03_Binary_Array_and_Matrix/08.cpp
@@ -3,3 +3,25 @@
 Array and Matrix
 
 */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 0, 1},
+        {0, 1, 0},
+        {1, 0, 1}
+    };
+
+    // Print the matrix
+    for (const auto& row : matrix) {
+        for (const auto& element : row) {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
