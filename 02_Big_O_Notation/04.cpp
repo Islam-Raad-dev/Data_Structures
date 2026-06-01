cad2ad02dd73e3d022cb0da2026374d34dacf9e0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-01 09:05:02 +0300

    Edit :  8

--- 02_Big_O_Notation/04.cpp
+++ 02_Big_O_Notation/04.cpp
@@ -1,5 +1,26 @@
 /*
 
-
+Time Complexity: O(n^2).
 
 */
+
+#include <iostream>
+using namespace std;
+
+void PrintPairs(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << "(" << arr[i] << ", " << arr[j] << ")" << endl;
+        }
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    PrintPairs(arr, n);
+    return 0;
+}
