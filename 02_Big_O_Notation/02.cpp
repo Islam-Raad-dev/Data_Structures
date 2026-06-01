9b01aeaafbf29dc5d1296e0ae1b902710a96d0ec
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-01 08:59:25 +0300

    Edit : 1

--- 02_Big_O_Notation/02.cpp
+++ 02_Big_O_Notation/02.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-          
-using namespace std;
-                    
-int main()     
-{      
 
+using namespace std;
+
+char GetLastCharacter(string S1)
+{
+    return S1[S1.length() - 1];
+}
+int main()
+{
+    string S1;
+
+    cout << "Enter a string: ";
+    cin >> S1;
+
+    cout << "The last character of the string is: " << GetLastCharacter(S1) << endl;
     return 0;
 }
