ca27484a9a7f45a9175ef2f59d621d645cdb1181
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-01 17:22:09 +0300

    Time Complexity: O(log n).

--- 02_Big_O_Notation/05.cpp
+++ 02_Big_O_Notation/05.cpp
@@ -7,3 +7,44 @@ Time Complexity: O(log n).
 #include <iostream>
 using namespace std;
 
+int BinarySearch(int arr[], int n, int target)
+{
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid; // Target found
+        }
+        else if (arr[mid] < target)
+        {
+            left = mid + 1; // Search in the right half
+        }
+        else
+        {
+            right = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Target not found
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 3;
+
+    int result = BinarySearch(arr, n, target);
+    if (result != -1)
+    {
+        cout << "Element found at index: " << result << endl;
+    }
+    else
+    {
+        cout << "Element not found in the array." << endl;
+    }
+    return 0;
+}
