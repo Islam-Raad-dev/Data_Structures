3251e51f6807498c974d5a837cd4a222f73cefaf
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-02 19:34:09 +0300

    Queue Swapping

--- 04_Stack_and_Queue/12.cpp
+++ 04_Stack_and_Queue/12.cpp
@@ -3,3 +3,42 @@
 Queue Swapping
 
 */
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue < int > MyQueue1;
+    queue < int > MyQueue2;
+
+    MyQueue1.push(10);
+    MyQueue1.push(20);
+    MyQueue1.push(30);
+    MyQueue1.push(40);
+
+    MyQueue2.push(50);
+    MyQueue2.push(60);
+    MyQueue2.push(70);
+    MyQueue2.push(80);
+
+    MyQueue1.swap(MyQueue2);
+
+    cout << "\nMyQueue1 = ";
+    while (!MyQueue1.empty())
+    {
+        cout << MyQueue1.front() << " ";
+        MyQueue1.pop();
+    }
+
+    cout << endl << "MyQueue2 = ";
+    while (!MyQueue2.empty())
+    {
+        cout << MyQueue2.front() << " ";
+        MyQueue2.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
