b0362d57847db72cb22862f497301d7fd15a29a7
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-02 19:29:14 +0300

    Edit : 9

--- 04_Stack_and_Queue/11.cpp
+++ 04_Stack_and_Queue/11.cpp
@@ -1,5 +1,33 @@
 /*
 
-Queue 
+Queue
 
 */
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    // Queue container declaration
+    queue<int> MyQueue;
+
+    // pushing elements into first stack
+    MyQueue.push(10);
+    MyQueue.push(20);
+    MyQueue.push(30);
+    MyQueue.push(40);
+
+    cout << "\nCount: " << MyQueue.size();
+    cout << "\nFront: " << MyQueue.front();
+    cout << "\nBack: " << MyQueue.back() << endl;
+
+    cout << "\nMyQueue = ";
+    while (!MyQueue.empty())
+    {
+        cout << MyQueue.front() << " ";
+        MyQueue.pop();
+    }
+
+    return 0;
+}
