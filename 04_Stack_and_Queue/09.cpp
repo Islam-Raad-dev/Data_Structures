28c428a3bd381aee234792093c19c9f9af13516b
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-02 18:37:20 +0300

    Edit : 5

--- 04_Stack_and_Queue/09.cpp
+++ 04_Stack_and_Queue/09.cpp
@@ -7,3 +7,26 @@ Stack
 #include <stack>
 using namespace std;
 
+int main()
+{
+    stack<int> stNumbers;
+
+    stNumbers.push(10);
+    stNumbers.push(20);
+    stNumbers.push(30);
+    stNumbers.push(40);
+    stNumbers.push(50);
+
+    cout << "Stack size: " << stNumbers.size() << endl;
+
+    cout << "Numbers are:\n";
+    
+    while (!stNumbers.empty())
+    {
+        cout << stNumbers.top() << "\n";
+
+        stNumbers.pop();
+    }
+
+    return 0;
+}
