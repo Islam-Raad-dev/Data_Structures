d003785defae7cc5bc6ded6bf5b6374fd7a105d0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-03 12:33:15 +0300

    Edit : 11

--- 05_Singly_Linked_List/15.cpp
+++ 05_Singly_Linked_List/15.cpp
@@ -3,3 +3,48 @@
 Singly Linked List : Find 
 
 */
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+};
+
+void InsertAtBeginning(Node *&Head, int value)
+{
+    Node *newNode = new Node();
+
+
+    newNode->value = value;
+    newNode->next = Head;
+
+    
+    Head = newNode;
+}
+
+void PrintList(Node *Head)
+{
+    while (Head != NULL)
+    {
+        cout << Head->value << endl;
+        Head = Head->next;
+    }
+}
+
+int main()
+{
+    Node * Head = NULL;
+
+    InsertAtBeginning(Head, 1);
+    InsertAtBeginning(Head, 2);
+    InsertAtBeginning(Head, 3);
+    InsertAtBeginning(Head, 4);
+
+    PrintList(Head);
+
+
+    return 0;
+}
