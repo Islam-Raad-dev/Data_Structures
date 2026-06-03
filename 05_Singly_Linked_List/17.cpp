63581440f0e8cc66b0fba044641cab2bfd1b71b3
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-03 13:38:45 +0300

    Edit : 21

--- 05_Singly_Linked_List/17.cpp
+++ 05_Singly_Linked_List/17.cpp
@@ -3,3 +3,82 @@
 Singly Linked List: Insert At End
 
 */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+};
+
+void InsertAtBeginning(Node *&Head, int value)
+{
+    Node *newNode = new Node();
+
+    newNode->value = value;
+    newNode->next = Head;
+
+    Head = newNode;
+}
+
+void PrintList(Node *Head)
+{
+    while (Head != NULL)
+    {
+        cout << Head->value << " ";
+        Head = Head->next;
+    }
+    cout << endl;
+}
+
+Node *Find(Node *Head, int value)
+{
+    while (Head != NULL)
+    {
+        if (Head->value == value)
+        {
+            return Head;
+        }
+        Head = Head->next;
+    }
+    return NULL;
+}
+
+void InsertAfter(Node *prev_Node, int value)
+{
+    if (prev_Node == NULL)
+    {
+        cout << "Node is NULL" << endl;
+        return;
+    }
+
+    Node *newNode = new Node();
+    newNode->value = value;
+    newNode->next = prev_Node->next;
+
+    prev_Node->next = newNode;
+}
+
+int main()
+{
+    Node *Head = NULL;
+
+    InsertAtBeginning(Head, 1);
+    InsertAtBeginning(Head, 2);
+    InsertAtBeginning(Head, 3);
+    InsertAtBeginning(Head, 4);
+
+    PrintList(Head);
+
+    Node *N1 = Find(Head, 2);
+
+    InsertAfter(N1, 500);
+
+    cout << "\nList after insertion: \n\n";
+    PrintList(Head);
+
+    return 0;
+}
