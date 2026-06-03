0b9523cc8e2fdd172459c99428285eb2de16f4a2
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-03 12:24:52 +0300

    Edit : 5

--- 05_Singly_Linked_List/14.cpp
+++ 05_Singly_Linked_List/14.cpp
@@ -10,5 +10,37 @@ class Node
 {
 public:
     int value;
-    Node * next;
+    Node *next;
 };
+
+void InsertAtBeginning(Node *&Head, int value)
+{
+    Node *newNode = new Node();
+    newNode->value = value;
+    newNode->next = Head;
+    Head = newNode;
+}
+
+void PrintList(Node *Head)
+{
+    while (Head != NULL)
+    {
+        cout << Head->value << endl;
+        Head = Head->next;
+    }
+}
+
+int main()
+{
+    Node * Head = NULL;
+
+    InsertAtBeginning(Head, 1);
+    InsertAtBeginning(Head, 2);
+    InsertAtBeginning(Head, 3);
+    InsertAtBeginning(Head, 4);
+
+    PrintList(Head);
+
+
+    return 0;
+}
