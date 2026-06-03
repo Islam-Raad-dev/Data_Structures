f16566b3bf6fd2ac81bb8dde6455cc291ec03ab0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-03 15:24:58 +0300

    Edit : 38

--- 05_Singly_Linked_List/20.cpp
+++ 05_Singly_Linked_List/20.cpp
@@ -3,3 +3,132 @@
 Singly Linked List: Delete Last Node
 
 */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+};
+
+void InsertAtBeginning(Node *&Head, int value)
+{
+    Node *newNode = new Node();
+
+    newNode->value = value;
+    newNode->next = Head;
+
+    Head = newNode;
+}
+
+void InsertAtEnd(Node *&Head, int value)
+{
+    Node *NewNode = new Node();
+    NewNode->value = value;
+    NewNode->next = NULL;
+
+    if (Head == NULL)
+    {
+        Head = NewNode;
+        return;
+    }
+
+    Node *LastNode = Head;
+    while (LastNode->next != NULL)
+    {
+        LastNode = LastNode->next;
+    }
+    LastNode->next = NewNode;
+}
+
+void PrintList(Node *Head)
+{
+    while (Head != NULL)
+    {
+        cout << Head->value << " ";
+        Head = Head->next;
+    }
+    cout << endl;
+}
+
+Node *Find(Node *Head, int value)
+{
+    while (Head != NULL)
+    {
+        if (Head->value == value)
+        {
+            return Head;
+        }
+        Head = Head->next;
+    }
+    return NULL;
+}
+
+void InsertAfter(Node *prev_Node, int value)
+{
+    if (prev_Node == NULL)
+    {
+        cout << "Node is NULL" << endl;
+        return;
+    }
+
+    Node *newNode = new Node();
+    newNode->value = value;
+    newNode->next = prev_Node->next;
+
+    prev_Node->next = newNode;
+}
+
+Node *DeleteNode(Node *Head, int value)
+{
+    if (Head == NULL)
+    {
+        return Head;
+    }
+
+    if (Head->value == value)
+    {
+        Node *temp = Head;
+        Head = Head->next;
+        delete temp;
+        return Head;
+    }
+
+    Node *current = Head;
+    while (current->next != NULL && current->next->value != value)
+    {
+        current = current->next;
+    }
+
+    if (current->next == NULL)
+    {
+        cout << "Value not found in the list." << endl;
+        return Head;
+    }
+
+    Node *temp = current->next;
+    current->next = current->next->next;
+    delete temp;
+
+    return Head;
+}
+
+Node *DeleteFirstNode(Node *Head)
+{
+    Node *Current = Head;
+
+    if (Head == NULL)
+    {
+        return Head;
+    }
+
+    Head = Current->next;
+    delete Current;
+
+    return Head;
+}
+
+int main()
