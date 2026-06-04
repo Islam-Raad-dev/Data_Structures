e31651a1d504be6c5d77468ace68358a06207bd0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-04 08:11:58 +0300

    Edit : 7

--- 06_Doubly_Linked_List/23.cpp
+++ 06_Doubly_Linked_List/23.cpp
@@ -3,3 +3,55 @@
 Double Linked List: Find Node
 
 */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+               
+public:
+    int Value;
+    Node *Next;
+    Node *Prev;
+};
+
+void InsertAtBeginning(Node *&Head, int Value)
+{
+    Node * NewNode = new Node();
+    NewNode->Value = Value;
+    NewNode->Next = Head;
+    NewNode->Prev = NULL;
+    
+    if(Head != NULL)
+    {
+        Head->Prev = NewNode;
+    }
+
+    Head = NewNode;
+
+}
+
+void PrintList(Node *Head)
+{
+    while(Head != NULL)
+    {
+        cout << Head->Value << " ";
+        Head = Head->Next;
+    }
+}
+
+int main()
+{
+    Node *Head = NULL;
+
+    InsertAtBeginning(Head, 5);
+    InsertAtBeginning(Head, 4);
+    InsertAtBeginning(Head, 3);
+    InsertAtBeginning(Head, 2);
+    InsertAtBeginning(Head, 1);
+
+    PrintList(Head);
+
+    return 0;
+}
