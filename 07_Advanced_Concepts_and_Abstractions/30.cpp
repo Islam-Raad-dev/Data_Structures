3cb256c0dcb559817d74f545627d5f8a3af488ff
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-04 11:16:36 +0300

    Edit : 46

--- 07_Advanced_Concepts_and_Abstractions/30.cpp
+++ 07_Advanced_Concepts_and_Abstractions/30.cpp
@@ -1,5 +1,14 @@
 /*
 
+Union:
 
+ A union is a data structure that can hold different types of data but 
+ only one type at a time. It is similar to a struct, but all members of a 
+ union share the same memory location. This means that the size of a union 
+ is determined by the size of its largest member. Unions are useful when 
+ you want to store different types of data in the same memory location, 
+ but you need to be careful when accessing the members of a union, as it 
+ can lead to undefined behavior if you access a member that is not 
+ currently active.
 
 */
