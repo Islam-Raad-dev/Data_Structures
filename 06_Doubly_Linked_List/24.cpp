9c17fe7ecd3fd3c57330bf181f09d18ed6dfc783
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-04 08:25:59 +0300

    Edit : 12

--- 06_Doubly_Linked_List/24.cpp
+++ 06_Doubly_Linked_List/24.cpp
@@ -1,5 +1,79 @@
 /*
 
-Double Linked List:
+Double Linked List: Insert After Node
 
 */
+/*
+
+Double Linked List: Find Node
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int Value;
+    Node *Next;
+    Node *Prev;
+};
+
+void InsertAtBeginning(Node *&Head, int Value)
+{
+    Node *NewNode = new Node();
+    NewNode->Value = Value;
+    NewNode->Next = Head;
+    NewNode->Prev = NULL;
+
+    if (Head != NULL)
+    {
+        Head->Prev = NewNode;
+    }
+
+    Head = NewNode;
+}
+
+void Find(Node *Head, int Value)
+{
+    while (Head != NULL)
+    {
+        if (Head->Value == Value)
+        {
+            cout << "Node found with value: " << Value << endl;
+            return;
+        }
+        Head = Head->Next;
+    }
+    cout << "Node not found with value: " << Value << endl;
+}
+
+void PrintList(Node *Head)
+{
+    while (Head != NULL)
+    {
+        cout << Head->Value << " ";
+        Head = Head->Next;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    Node *Head = NULL;
+
+    InsertAtBeginning(Head, 5);
+    InsertAtBeginning(Head, 4);
+    InsertAtBeginning(Head, 3);
+    InsertAtBeginning(Head, 2);
+    InsertAtBeginning(Head, 1);
+    PrintList(Head);
+
+    Find(Head, 3);
+    Find(Head, 6);
+
+    return 0;
+}
