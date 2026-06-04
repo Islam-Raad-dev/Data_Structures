899fd18b9f4d746674bbb687ada90cb5efd9d3c8
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-04 09:35:52 +0300

    Edit : 22

--- 06_Doubly_Linked_List/25.cpp
+++ 06_Doubly_Linked_List/25.cpp
@@ -3,3 +3,90 @@
 Double Linked List: Insert At End
 
 */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int Value;
+    Node *Next;
+    Node *Prev;
+};
+
+void InsertAtBeginning(Node *&Head, int Value)
+{
+    Node *NewNode = new Node();
+    NewNode->Value = Value;
+    NewNode->Next = Head;
+    NewNode->Prev = NULL;
+
+    if (Head != NULL)
+    {
+        Head->Prev = NewNode;
+    }
+
+    Head = NewNode;
+}
+
+Node* Find(Node *Head, int Value)
+{
+    while (Head != NULL)
+    {
+        if (Head->Value == Value)
+        {
+            return Head;
+        }
+        Head = Head->Next;
+    }
+    return NULL;
+}
+
+void InsertAfter(Node *&Current, int Value)
+{
+    Node * NewNode = new Node();
+    NewNode->Value = Value;
+    NewNode->Next = Current->Next;
+    NewNode->Prev = Current;
+
+    if(Current->Next != NULL)
+    {
+        Current->Next->Prev = NewNode;
+    }
+
+    Current->Next = NewNode;
+
+}
+
+void PrintList(Node *Head)
+{
+    while (Head != NULL)
+    {
+        cout << Head->Value << " ";
+        Head = Head->Next;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    Node *Head = NULL;
+
+    InsertAtBeginning(Head, 5);
+    InsertAtBeginning(Head, 3);
+    InsertAtBeginning(Head, 2);
+    InsertAtBeginning(Head, 1);
+    PrintList(Head);
+
+    Node * N1 = Find(Head, 3);
+
+    InsertAfter(N1, 4);
+
+    cout << "After Inserting: " << endl;
+    PrintList(Head);
+
+    return 0;
+}
